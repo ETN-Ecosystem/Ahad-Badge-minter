900ba3a0268f1dff7c1d4590d5714fc2bcd2d016
jasonpeters.ton <30388861+jayp89@users.noreply.github.com>
2024-10-30 04:19:54 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@
 import MintSBT from "@/components/MintSBT";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-const MANIFEST_URL = 'https://gateway.pinata.cloud/ipfs/QmZd2pBynX8ieaBJHuEhkMQD9fCvCboKJsbEbpVJ7TLAEr';
+const MANIFEST_URL = 'https://etneco.ethio-tech.com/tonconnect-manifest.json';
 
 export default function Home() {
   return (
