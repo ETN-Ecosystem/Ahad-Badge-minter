89dc511e35d69e22f844f463dde5ccea656167df
jasonpeters.ton <30388861+jayp89@users.noreply.github.com>
2024-11-21 23:02:31 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@
 import MintSBT from "@/components/MintSBT";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-const MANIFEST_URL = 'https://emerald-efficient-macaw-234.mypinata.cloud/ipfs/QmYW2TZSPpYmhRqWgoNPeyUymDhSruaY1fpVw5JsJdedpx';
+const MANIFEST_URL = 'https://emerald-efficient-macaw-23.mypinata.cloud/ipfs/QmYW2TZSPpYmhRqWgoNPeyUymDhSruaY1fpVw5JsJdedpx';
 
 export default function Home() {
   return (
